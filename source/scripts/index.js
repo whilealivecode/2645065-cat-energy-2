47c5d7162a4a3d9e2a3cee78e3233800c29993f6
Ирина <whilealivecode@yandex.ru>
2026-03-08 02:02:17 +0300

    Добавляет фоллбэк для интерактивной карты и все декоративные линии к доп. товарам

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
-/* в этот файл добавляет скрипты*/
+document.body.classList.add('js');
+const mapInteractive = document.querySelector('.map--interactive');
+const mapFallback = document.querySelectorAll('.map--fallback')[1]; // Картинка-фоллбэк в <picture>
+
+// Показываем iframe, скрываем картинку-фоллбэк (по умолчанию)
+mapInteractive.style.display = 'block';
+mapFallback.style.display = 'none';
+
