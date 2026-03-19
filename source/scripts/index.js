252df6f1d7fab14a14a683e2f7661444cbb59355
Ирина <whilealivecode@yandex.ru>
2026-03-19 13:15:57 +0300

    Адаптивная вёрстка сайта Cat Energy (#16)
    * Исправляет paddings у контактов
    
    * Исправляет навигацию и hero на десктопе на главной странице.
    
    * Закончена вёрстка блока Programs
    
    * Делает мобильную и планшетную версию блока Инструкции
    
    * Закончена вёрстка блока Instructions
    
    * Закончена вёрстка футера
    
    * Делает интерактивную карту
    
    * Добавляет интерактивную карту. Закончена вёрстка блока Контакты.
    
    * Начата вёрстка блока  дополнительных товаров
    
    * Делаёт вёртстку дополнительных товаров и подарка
    
    * Делает зелёные декоративные линии
    
    * Делает вёрстку дополнительных товаров (mobile)
    
    * Добавляет фоллбэк для интерактивной карты и все декоративные линии к доп. товарам
    
    * Доделывает вёрстку дополнительных товаров и блока "Gift"
    
    * Добавляет вёрстку блока Subscribe
    
    * Делает вёрстку главных товаров каталога
    
    * Доделывает вёрстку блока главных товаров и заголовка каталога
    
    * Делает вёрстку блока Example  (включая слайдер)
    
    * Доделывает вёрстку блока Example (включая слайдер)
    
    * Делаёт вёрстку блока Hero
    
    * Доделывает вёрстку блока hero
    
    * Делает вёрстку главной навигации
    
    * Доделывает вёрстку блока Навигация
    
    * Предотвращает переполнение текстом на главной странице
    
    * Проверяет вёрстку на переполнение и недополнение в мобильной версии
    
    * Предотвращает переполнение главной страницы в планшетной версии
    
    * Предотвращает переполнение на странице каталога.
    
    * Предотвращает переполнение на  странице каталога (все брейкпоинты)
    
    * Предотвращает переполнение на странице index.html (все брейкпоинты).
    
    * Проверка на кросбраузерность
    
    * Исправляет ошибки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,17 @@
-/* в этот файл добавляет скрипты*/
+// Если JS загрузился, то добавляем к body класс .js
+document.body.classList.add('js');
+
+const mapInteractive = document.querySelector('.map--interactive');
+const mapFallback = document.querySelectorAll('.map--fallback')[1];
+
+// Показываем интерактивную карту, скрываем картинку-фоллбэк (по умолчанию)
+mapInteractive.style.display = 'block';
+mapFallback.style.display = 'none';
+
+const mainNavList = document.querySelector('.main-navigation__list');
+const mainNavToggle = document.querySelector('.main-navigation__toggle');
+
+mainNavToggle.addEventListener('click', () => {
+  mainNavList.classList.toggle('main-navigation__list--hidden');
+  mainNavToggle.classList.toggle('main-navigation__toggle--closed');
+});
