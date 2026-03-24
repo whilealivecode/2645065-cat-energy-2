a7beaf03fa3965688fcaca611663df2922c6c21f
Ирина <whilealivecode@yandex.ru>
2026-03-24 18:38:13 +0300

    Исправляет карту

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,13 +1,6 @@
 // Если JS загрузился, то добавляем к body класс .js
 document.body.classList.add('js');
 
-const mapInteractive = document.querySelector('.map--interactive');
-const mapFallback = document.querySelectorAll('.map--fallback')[1];
-
-// Показываем интерактивную карту, скрываем картинку-фоллбэк (по умолчанию)
-mapInteractive.style.display = 'block';
-mapFallback.style.display = 'none';
-
 const mainNavList = document.querySelector('.main-navigation__list');
 const mainNavToggle = document.querySelector('.main-navigation__toggle');
 
